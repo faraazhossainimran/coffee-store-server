8730ea87b28e652a74913e2a940e30d83a57d0d8
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-10-20 17:06:28 +0600

    get endpoint for the coffee added

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const coffeeCollection = client.db("coffeeDB").collection("coffee");
+    // get coffee
+    app.get('/coffee', async(req, res) => {
+        const cursor = coffeeCollection.find();
+        const result = await cursor.toArray();
+        res.send(result)
+    })
+
     // added my code || post request
     app.post('/coffee', async(req, res) => {
         const newCoffee = req.body;
