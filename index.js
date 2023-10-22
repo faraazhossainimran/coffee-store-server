b9ccf7a1691c6e5eee0629d81c66a3a0644dda63
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-10-22 17:58:04 +0600

    used patch to update a data

--- index.js
+++ index.js
@@ -79,12 +79,39 @@ async function run() {
       res.send(result);
     });
     // user related apis
+    app.get('/user', async(req, res)=> {
+        const cursor = userCollection.find();
+        const users = await cursor.toArray();
+        res.send(users)
+    })  
+
     app.post('/user', async(req, res)=> {
         const user = req.body;
         const result = await userCollection.insertOne(user);
         console.log(user);
         res.send(result)
     })
+
+    app.patch('/user', async(req, res)=> {
+        const user = req.body;
+        const filter = { email: user.email };
+        const updateDoc = {
+            $set: {
+              lastLoggedAt: user.lastLoggedAt
+            },
+          };
+          const result = await userCollection.updateOne(filter, updateDoc);
+          console.log(result);
+          res.send(result)
+    })
+    app.delete('/user/:id', async(req, res)=> {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await userCollection.deleteOne(query)
+        console.log(result);
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
